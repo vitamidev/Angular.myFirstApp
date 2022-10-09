2f66cccbecc82466d17a4da2a427a56371845438
vitamidev <vitamidev@yahoo.com>
2022-10-09 12:04:45 +0200

    commit avec tortoiseGit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasicComponent } from './basic/basic.component';
+import { DeviseComponent } from './devise/devise.component';
+import { LoginComponent } from './login/login.component';
+import { XyComponent } from './xy/xy.component';
+import { ZzComponent } from './zz/zz.component';
 
-const routes: Routes = [];
+// const routes: Routes = [];
 
+const routes: Routes = [
+ // { path: 'ngr-welcome', component: WelcomeComponent },
+  //{ path: '', redirectTo: '/ngr-welcome', pathMatch: 'full'},
+  { path: 'ngr-login', component: LoginComponent },
+  { path: 'ngr-basic', component: BasicComponent },
+  { path: 'ngr-xy', component: XyComponent },
+  { path: 'ngr-zz', component: ZzComponent },
+  { path: 'ngr-devise', component: DeviseComponent },
+  //{ path: 'ngr-demo', component: DemoComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BasicComponent } from './basic/basic.component';
+import { CalculatriceComponent } from './basic/calculatrice/calculatrice.component';
+import { TvaComponent } from './basic/tva/tva.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DeviseComponent } from './devise/devise.component';
+import { XyComponent } from './xy/xy.component';
+import { ZzComponent } from './zz/zz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BasicComponent,
+    CalculatriceComponent,
+    TvaComponent,
+    LoginComponent,
+    DeviseComponent,
+    XyComponent,
+    ZzComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
